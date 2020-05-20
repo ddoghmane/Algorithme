015669cd4234022d002d1a0b8b5f0be62112501c
Doghmane Daoud <Doghmane.daoud@gmail.com>
2020-05-20 03:51:24 +0200

    changement de nomdedossier

---
+++ Exercice-Dalgo/exercice-algo1/Palyndrome.java
@@ -0,0 +1,31 @@
+
+public class Palyndrome {
+  
+    public static void main(String args[]){
+      
+        int[] nombres = {0, 2, 113, 11, 1443, 1441, 1654, 19891};
+      
+        for(int nombre: nombres){
+            System.out.println(nombre +" est un palindrome? " 
+             + estPalindrome(nombre));
+        }             
+    }
+
+    private static boolean estPalindrome(int nombre) {
+        if(nombre == inverse(nombre)){
+            return true;
+        }
+        return false;
+    }
+  
+    private static int inverse(int nombre){
+        int inverse = 0;
+        while(nombre != 0){
+         inverse = inverse*10 + nombre%10; 
+          nombre = nombre/10;
+        }
+        return inverse;
+    }
+}
+
+
---
+++ Exercice-Dalgo/exercice-algo1/pointApp.java
@@ -0,0 +1,15 @@
+
+
+	public class pointApp
+	{ public static void main (String args[])
+	{ Point a = new Point ('C', 2.5) ; a.affiche() ;
+	Point b = new Point ('D', 5.25) ; b.affiche() ;
+	b.translate(2.25) ;
+	b.affiche() ; }
+	
+
+		// TODO Auto-generated method stub
+
+	}
+
+
